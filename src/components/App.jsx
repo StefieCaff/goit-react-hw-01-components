87e4e6b722e7e5b718395c37065a2068aa016abe
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-03-24 18:32:22 -0700

    styled-components broken

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,19 @@
-// import user from 'data/user.json'
-// import { Profile } from './User/Profile';
+import user from 'data/user.json'
+import { Profile } from './User/Profile';
 
-// import data from 'data/statistics.json';
-// import { Statistics } from './Statistics/Statistics';
+import data from 'data/statistics.json';
+import { Statistics } from './Statistics/Statistics';
 
 
-import friends from 'data/friends.json';
-import { FriendList } from './Friends/FriendList';
+// import friends from 'data/friends.json';
+// import { FriendList } from './Friends/FriendList';
+
 export const App = () => {
 
   return (
     
     <>
-      {/* <Profile
+       <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
@@ -21,9 +22,9 @@ export const App = () => {
       />
      
       <Statistics stats={data} />
-      <Statistics title= 'Upload Stats' stats={data} /> */}
+      <Statistics title= 'Upload Stats' stats={data} /> 
       
-      <FriendList friends={friends} />
+      {/* <FriendList friends={friends} /> */}
 
     </>
   );
